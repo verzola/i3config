a970f621c03f24e3c28686211531679ef6cdfe1a
Gustavo Verzola <verzola@gmail.com>
2022-06-28 21:52:05 -0300

    add polybar launch script

--- apply_configs.sh
+++ apply_configs.sh
@@ -2,3 +2,4 @@
 cp .config/i3/config ~/.config/i3/config 
 cp .config/i3status/config ~/.config/i3status/config 
 cp .config/polybar/config.ini ~/.config/polybar/config.ini
+cp .config/polybar/launch.sh ~/.config/polybar/launch.sh
