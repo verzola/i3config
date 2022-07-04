7b25c76b0f9970ff9e5451178a142a8a22cfbea8
Gustavo Verzola <verzola@gmail.com>
2022-07-03 22:20:00 -0300

    tweak rofi config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -r --config=~/.config/polybar/config.ini example
+polybar -r --config=~/.config/polybar/config.ini main
