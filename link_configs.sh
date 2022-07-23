a25d2bbcbd61d32222b9d5e3a581de9a6de359d8
Gustavo Verzola <verzola@gmail.com>
2022-07-22 21:59:43 -0300

    tweak configs

--- link_configs.sh
+++ link_configs.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-rm ~/.config/i3/config 
-rm ~/.config/i3status/config
+rm ~/.config/i3/config
 rm ~/.config/polybar/config.ini
 rm ~/.config/polybar/launch.sh
 rm ~/.config/rofi/config.rasi
-rm ~/.config/compton.conf
+rm ~/.config/picom/picom.conf
+rm ~/.config/dunst/dunstrc
 
-ln -s ~/projects/verzola/i3config/.config/i3/config ~/.config/i3/config 
-ln -s ~/projects/verzola/i3config/.config/i3status/config ~/.config/i3status/config 
-ln -s ~/projects/verzola/i3config/.config/polybar/config.ini ~/.config/polybar/config.ini
-ln -s ~/projects/verzola/i3config/.config/polybar/launch.sh ~/.config/polybar/launch.sh
-ln -s ~/projects/verzola/i3config/.config/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -s ~/projects/verzola/i3config/.config/compton.conf ~/.config/compton.conf
+mkdir -p ~/.config/i3/
+mkdir -p ~/.config/polybar/
+mkdir -p ~/.config/rofi/
+mkdir -p ~/.config/picom/
+mkdir -p ~/.config/dunst/
+mkdir -p ~/.local/share/rofi/themes/
+
+ln -s ~/i3config/.config/i3/config ~/.config/i3/config
+ln -s ~/i3config/.config/polybar/config.ini ~/.config/polybar/config.ini
+ln -s ~/i3config/.config/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -s ~/i3config/.config/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -s ~/i3config/.config/picom/picom.conf ~/.config/picom/picom.conf
+ln -s ~/i3config/.config/dunst/dunstrc ~/.config/dunst/dunstrc
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt install -y i3 i3status nitrogen rofi polybar compton compton-conf
-mkdir -p ~/.config/i3/
-mkdir -p ~/.config/i3status/
-mkdir -p ~/.config/polybar/
-mkdir -p ~/.config/rofi/
-mkdir -p ~/.local/share/rofi/themes/
